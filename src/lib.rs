b400c4164bc48b0b38c41aae2f2d4857e0797c04
Deveus <raffree.dev@gmail.com>
2021-12-25 23:11:46 +0200

    Assert

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,181 @@
+
+use core::borrow::Borrow;
+use core::fmt::Debug;
+
+use colors::*;
+
+#[cfg(not(test))]
+mod colors {
+    pub const RED: &'static str = "\x1B[31m";
+    pub const BOLD: &'static str = "\x1B[1m";
+    pub const RESET: &'static str = "\x1B[0m";
+}
+
 #[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+mod colors {
+    pub const RED: &'static str = "";
+    pub const BOLD: &'static str = "";
+    pub const RESET: &'static str = "";
+}
+
+#[derive(Debug)]
+pub struct Assert<'a, C> {
+    pub component: &'a C,
+    pub component_name: Option<&'a str>,
+    pub location: Option<String>,
+    pub description: Option<&'a str>,
+}
+
+#[derive(Debug)]
+pub struct AssertionFailure<'a, C: 'a> {
+    component: &'a C,
+    expected: Option<String>,
+    actual: Option<String>,
+}
+
+pub trait AssertInfo<'a>{
+    fn component_name(&self) -> Option<&'a str>;
+    fn location(&self) -> Option<String>;
+    fn description(&self) -> Option<&'a str>;
+}
+impl<'a, C> Assert<'a, C> {
+    pub fn that(component: &'a C) -> Assert<'a, C> {
+        Assert {
+            component: component,
+            component_name: None,
+            location: None,
+            description: None,
+        }       
+    }
+    pub fn with_location(self, location: String) -> Self {
+        let mut assert = self;
+        assert.location = Some(location);
+        assert
+    }
+    pub fn named(self, component_name: &'a str) -> Self {
+        let mut assert = self;
+        assert.component_name = Some(component_name);
+        assert
+    }
+}
+impl<'a, T> AssertInfo<'a> for Assert<'a, T> {
+    fn component_name(&self) -> Option<&'a str> {
+        self.component_name
+    }
+
+    fn location(&self) -> Option<String> {
+        self.location.clone()
+    }
+
+    fn description(&self) -> Option<&'a str> {
+        self.description
+    }
+}
+
+impl<'a, A: AssertInfo<'a>> AssertionFailure<'a, A> {
+    pub fn from(component: &'a A) -> AssertionFailure<'a, A> {
+        AssertionFailure {
+            component: component,
+            expected: None,
+            actual: None,
+        }
+    }
+
+    pub fn with_expected(&mut self, expected: String) -> &mut Self {
+        let mut assertion = self;
+        assertion.expected = Some(expected);
+
+        assertion
+    }
+
+    pub fn with_actual(&mut self, actual: String) -> &mut Self {
+        let mut assertion = self;
+        assertion.actual = Some(actual);
+
+        assertion
+    }
+
+   
+    pub fn fail(&mut self) {
+        if !self.expected.is_some() || !self.actual.is_some() {
+            panic!("Invalid assertion info");
+        }
+
+        let location = self.location_formatted();
+        let component_name = self.component_name_formatted();
+        let description = self.description_formatted();
+
+        panic!("{}{}\n\t{}expected: {}\n\t but was: {}{}\n{}",
+                       description,
+                       component_name,
+                       RED,
+                       self.expected.clone().unwrap(),
+                       self.actual.clone().unwrap(),
+                       RESET,
+                       location)
+    }
+
+
+    fn fail_with_message(&mut self, message: String) {
+        let location = self.location_formatted();
+        let component_name = self.component_name_formatted();
+        let description = self.description_formatted();
+
+        panic!("{}{}\n\t{}{}{}\n{}",
+                       description,
+                       component_name,
+                       RED,
+                       message,
+                       RESET,
+                       location)
+    }
+
+    fn location_formatted(&self) -> String {
+        match self.component.location() {
+            Some(value) => format!("\n\t{}at location: {}{}\n", BOLD, value, RESET),
+            None => "".to_string(),
+        }
+    }
+
+    fn description_formatted(&self) -> String {
+        match self.component.description() {
+            Some(value) => format!("\n\t{}:{}{}", BOLD, value, RESET),
+            None => "".to_string(),
+        }
+    }
+
+    fn component_name_formatted(&self) -> String {
+        match self.component.component_name() {
+            Some(value) => format!("\n\t{}for component [{}]{}", BOLD, value, RESET),
+            None => "".to_string(),
+        }
+    }
+}
+
+impl<'a, C> Assert<'a, C>
+    where C: Debug + PartialEq
+{
+    pub fn is_equal_to<E: Borrow<C>>(&mut self, expected: E) {
+        let subject = self.component;
+        let borrowed_expected = expected.borrow();
+
+        if !subject.eq(borrowed_expected) {
+            AssertionFailure::from(self)
+                .with_expected(format!("<{:?}>", borrowed_expected))
+                .with_actual(format!("<{:?}>", subject))
+                .fail();
+        }
+    }
+
+    pub fn is_not_equal_to<E: Borrow<C>>(&mut self, expected: E) {
+        let subject = self.component;
+        let borrowed_expected = expected.borrow();
+
+        if subject.eq(borrowed_expected) {
+            AssertionFailure::from(self)
+                .with_expected(format!("<{:?}> to not equal <{:?}>", subject, borrowed_expected))
+                .with_actual(format!("equal"))
+                .fail();
+        }
     }
 }
